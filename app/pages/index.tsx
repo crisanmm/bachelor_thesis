6ceee858990056dab33c3a8a96197b43573a67c9
mcrisan5537 <crisanmihai5537@gmail.com>
2021-02-24 12:29:11 +0200

    index.tsx: Refactor dynamic import

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,18 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
 import * as THREE from "three";
+import { useState } from "react";
 import { Canvas, useFrame, useResource, useThree } from "react-three-fiber";
 import { Layout } from "../components";
-
-let OrbitControls;
-import("three/examples/jsm/controls/OrbitControls").then((module) => {
-  OrbitControls = module.OrbitControls;
-});
 
 export default function Index() {
   return (
     <Layout>
-      <Canvas style={{ minHeight: "50vh" }}>
+      <Canvas style={{ minHeight: "100vh" }}>
         {(state) => {
           state.gl.setClearAlpha(0.1);
           return (
@@ -42,6 +38,7 @@ function PointLight() {
 
 function handleOnClick(e: any) {
   e.object.material.color.set(Math.random() * 0xffffff);
+  e.stopPropagation();
 }
 
 function Cube({ position }: { position: [number, number, number] }) {
@@ -76,18 +73,35 @@ function Sphere({ position }: { position: [number, number, number] }) {
   );
 }
 
-function Controls() {
+// function replacer(key, value) {
+//   if (typeof value === "object" && key != "") {
+//     return undefined;
+//   }
+//   return value;
+// }
+
+const Controls = () => {
   const {
     camera,
     gl: { domElement: canvasElement },
   } = useThree();
 
-  const controls = new OrbitControls(camera, canvasElement);
-  controls.enableDamping = true;
+  let [controls, setControls]: [any, any] = useState({});
 
   useFrame(() => {
-    controls.update();
+    if (Object.keys(controls).length == 0) {
+      (async () => {
+        const module = await import("three/examples/jsm/controls/OrbitControls");
+        setControls(new module.OrbitControls(camera, canvasElement));
+        controls.enableDamping = true;
+      })();
+    } else {
+      controls.enableDamping = true;
+      if (controls.update) {
+        controls.update();
+      }
+    }
   });
 
   return <></>;
-}
+};
