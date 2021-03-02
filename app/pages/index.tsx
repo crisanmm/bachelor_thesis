8937005e323c841000693ac11f57a26b6ade32e4
mcrisan5537 <crisanmihai5537@gmail.com>
2021-03-02 22:13:37 +0200

    pages/: add Index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,107 +1,102 @@
-import Head from "next/head";
-import Link from "next/link";
-import * as THREE from "three";
-import { useState } from "react";
-import { Canvas, useFrame, useResource, useThree } from "react-three-fiber";
-import { Layout } from "../components";
+import Head from 'next/head';
+import Link from 'next/link';
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas, useThree, useFrame, useResource, useLoader } from 'react-three-fiber';
+import * as THREE from 'three';
+import { TextureLoader } from 'three';
 
-export default function Index() {
-  return (
-    <Layout>
-      <Canvas style={{ minHeight: "100vh" }}>
-        {(state) => {
-          state.gl.setClearAlpha(0.1);
-          return (
-            <>
-              <Controls />
-              <PointLight />
-              <Cube position={[3, 0, 0]} />
-              <Sphere position={[-3, 0, 0]} />
-            </>
-          );
-        }}
-      </Canvas>
-    </Layout>
-  );
-}
+import { Layout, SplitPane, ChatBox } from '../components/html';
+import { OrbitControls } from '../components/three';
 
-function PointLight() {
-  const light = useResource() as any;
-
+const Index = () => {
   return (
     <>
-      <pointLight ref={light} args={[0xffffff, 1, 25]} position={[0, 0, 10]} />
-      {light.current && <pointLightHelper args={[light.current, 1, 0xff0000]} />}
+      <Head>
+        <title>think-in</title>
+      </Head>
+      <Layout>
+        <SplitPane
+          percentage={60}
+          left={
+            <Canvas camera={{ fov: 60, position: [0, 0, 30] }}>
+              <OrbitControls />
+
+              <ambientLight args={[0xc3c3c3, 0.5]} />
+              {/* <pointLight args={[0xffffff, 0.6, 50]} position={[0, 10, -25]} /> */}
+              <directionalLight args={[0xffffff, 0.2]} />
+
+              <Suspense fallback={<></>}>
+                <axesHelper args={[50]} />
+                <Platform />
+                <PlatformWall />
+                <Plane />
+              </Suspense>
+            </Canvas>
+          }
+          right={
+            <>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                }}
+              >
+                <video src='screensaver.mp4' style={{ width: '100%' }} controls></video>
+                <ChatBox
+                  personNameList={['person1', 'person2', 'person3']}
+                  messageList={[
+                    { isMessageMine: true, content: 'mesaj1' },
+                    { isMessageMine: false, content: 'mesaj2' },
+                    { isMessageMine: false, content: 'mesaj3' },
+                    { isMessageMine: true, content: 'mesaj4' },
+                    { isMessageMine: true, content: 'mesaj5' },
+                  ]}
+                />
+              </div>
+            </>
+          }
+        />
+      </Layout>
     </>
   );
-}
-
-function handleOnClick(e: any) {
-  e.object.material.color.set(Math.random() * 0xffffff);
-  e.stopPropagation();
-}
-
-function Cube({ position }: { position: [number, number, number] }) {
-  const cube = useResource() as any;
-
-  useFrame((state, delta) => {
-    cube.current.rotation.x += delta;
-    cube.current.rotation.y += delta;
-  });
-
-  return (
-    <mesh ref={cube} position={[...position]} onClick={handleOnClick}>
-      <boxBufferGeometry args={[2, 2, 2]} />
-      <meshLambertMaterial />
-    </mesh>
-  );
-}
-
-function Sphere({ position }: { position: [number, number, number] }) {
-  const sphere = useResource() as any;
-
-  useFrame((state, delta) => {
-    sphere.current.rotation.x += delta;
-    sphere.current.rotation.y += delta;
-  });
-
-  return (
-    <mesh ref={sphere} position={[...position]} onClick={handleOnClick}>
-      <sphereBufferGeometry args={[1, 100, 100]} />
-      <meshPhongMaterial />
-    </mesh>
-  );
-}
-
-// function replacer(key, value) {
-//   if (typeof value === "object" && key != "") {
-//     return undefined;
-//   }
-//   return value;
-// }
-
-const Controls = () => {
-  const {
-    camera,
-    gl: { domElement: canvasElement },
-  } = useThree();
-
-  let [controls, setControls]: [any, any] = useState({});
-
-  useFrame(() => {
-    if (Object.keys(controls).length == 0) {
-      (async () => {
-        const module = await import("three/examples/jsm/controls/OrbitControls");
-        setControls(new module.OrbitControls(camera, canvasElement));
-        controls.enableDamping = true;
-      })();
-    } else {
-      controls.enableDamping = true;
-      if (controls.update) {
-        controls.update();
-      }
-    }
-  });
-
-  return <></>;
 };
+
+const Platform = () => {
+  return (
+    <mesh position={[0, 1, -26]}>
+      <boxBufferGeometry args={[50, 2, 8]} />
+      <meshStandardMaterial color={0xcc0000} />
+    </mesh>
+  );
+};
+
+const PlatformWall = () => {
+  const texturePicture = useLoader(THREE.TextureLoader, 'windows-xp.jpg');
+
+  const material = useResource();
+  const textureVideo = new THREE.VideoTexture(document.querySelector('video'));
+
+  return (
+    <mesh position={[0, 16, -30]} rotation={[0, 0, 0]}>
+      <planeBufferGeometry args={[50, 28, 8]} />
+      <meshBasicMaterial ref={material} map={textureVideo} side={THREE.DoubleSide} />
+    </mesh>
+  );
+};
+
+const Plane = () => {
+  const planeGeometry: any = useResource();
+  //   planeGeometry.current?.translate(5, 0, 0);
+
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry ref={planeGeometry} args={[50, 60]} />
+      <meshStandardMaterial color={0x006994} side={THREE.DoubleSide} />
+    </mesh>
+  );
+};
+
+export default Index;
