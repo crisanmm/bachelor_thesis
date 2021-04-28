ebcbb77e4affbf71f93815372d36727bc391062c
mcrisan5537 <crisanmihai5537@gmail.com>
2021-04-28 19:58:59 +0300

    Check for empty string before executing kill v2

--- app/BE/stage_server/scripts/application-stop.sh
+++ app/BE/stage_server/scripts/application-stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pid=`ps aux | grep "node out/index.js" | grep -v grep | tr -s " " " " | cut -d " " -f2`
 # if string is not empty
-if [ -n $pid ]
+if [ -n "${pid}" ]
 then
     kill -9 $pid
 fi
