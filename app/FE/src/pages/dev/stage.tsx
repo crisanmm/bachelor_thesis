7ad1b10a5b602abed4a23abf35b225db0e85b377
mcrisan5537 <crisanmihai5537@gmail.com>
2021-05-04 19:40:02 +0300

    Delete dev pages

--- app/FE/src/pages/dev/stage.tsx
+++
@@ -1,6 +0,0 @@
-// import { Stage } from '@components/index';
-
-// const StagePage = () => <Stage style={{ height: '100vh', width: '100vw' }} />;
-// export default StagePage;
-
-export default () => <span>a</span>;
