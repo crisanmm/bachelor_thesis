3ce451765450c646b6ef49e47142f915e89d8689
mcrisan5537 <crisanmihai5537@gmail.com>
2021-05-10 21:18:43 +0300

    More explicit application-stop.sh & package.json module type

--- app/BE/stage_server/scripts/application-stop.sh
+++ app/BE/stage_server/scripts/application-stop.sh
@@ -4,4 +4,7 @@ pid=`ps aux | grep "node out/index.js" | grep -v grep | tr -s " " " " | cut -d "
 if [ -n "${pid}" ]
 then
     kill -9 $pid
+    echo Killed process with pid $pid
+else
+    echo Found no process with pid $pid
 fi
