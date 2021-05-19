65a149a0ce5ae091066b560b7bb32d0518e2fb87
mcrisan5537 <crisanmihai5537@gmail.com>
2021-05-19 14:15:03 +0300

    FE: Add to AccountContext upload avatar & change email & change password & delete user functionality

--- app/FE/src/contexts/AccountContext/AccountContext.tsx
+++ app/FE/src/contexts/AccountContext/AccountContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect } from 'react';
+import axios from 'axios';
 import {
   CognitoUserSession,
   ISignUpResult,
@@ -7,6 +8,8 @@ import {
 } from 'amazon-cognito-identity-js';
 import Amplify, { Auth } from 'aws-amplify';
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+
+const API_URI = 'https://api.think-in.me/dev';
 
 Amplify.configure({
   Auth: {
@@ -41,16 +44,22 @@ const signUp: SignUp = async (email, password, firstName, lastName, picture) =>
   });
 
 interface UpdateUserAttributes {
+  (attributes: { [key: string]: string }): Promise<string>;
+}
+
+const updateUserAttributes: UpdateUserAttributes = async (attributes) => {
+  const user = (await Auth.currentUserPoolUser()) as CognitoUser;
+  return Auth.updateUserAttributes(user, attributes);
+};
+
+interface VerifyEmailAttribute {
   (
-    user: CognitoUser,
-    attributes: {
-      [key: string]: string;
-    },
+    code: string, // verification code
   ): Promise<string>;
 }
 
-const updateUserAttributes: UpdateUserAttributes = async (user, attributes) =>
-  Auth.updateUserAttributes(user, attributes);
+const verifyEmailAttribute: VerifyEmailAttribute = (code) =>
+  Auth.verifyCurrentUserAttributeSubmit('email', code);
 
 interface SignIn {
   (email: string, password: string): Promise<CognitoUserSession>;
@@ -67,6 +76,15 @@ interface SignOut {
 
 const signOut: SignOut = () => Auth.signOut();
 
+interface ChangePassword {
+  (oldPassword: string, newPassword: string): Promise<void>;
+}
+
+const changePassword: ChangePassword = async (oldPassword, newPassword) => {
+  const user = (await Auth.currentUserPoolUser()) as CognitoUser;
+  await Auth.changePassword(user, oldPassword, newPassword);
+};
+
 interface ForgotPasswordSendCode {
   (email: string): Promise<CodeDeliveryDetails>;
 }
@@ -80,22 +98,81 @@ interface ForgotPasswordReset {
 const forgotPasswordReset: ForgotPasswordReset = async (email, code, newPassword) =>
   Auth.forgotPasswordSubmit(email, code, newPassword);
 
+type UploadAvatarResponse = {
+  success: boolean;
+  avatarURI: string;
+  error: string;
+};
+interface UploadAvatarOnSignUp {
+  // userId - uuid v4 user id
+  // avatarDataURI - avatar data URI
+  (userId: string, avatarDataURI: string): Promise<UploadAvatarResponse>;
+}
+
+const uploadAvatarOnSignUp: UploadAvatarOnSignUp = async (userId, avatarDataURI) => {
+  const response = await axios.post(
+    `${API_URI}/avatars`,
+    { userId, avatarDataURI },
+    { headers: { 'Content-Type': 'application/json' } },
+  );
+  return response.data;
+};
+
+interface UploadAvatarType {
+  // avatarDataURI - avatar data URI
+  (avatarDataURI: string): Promise<UploadAvatarResponse>;
+}
+
+const uploadAvatar: UploadAvatarType = async (avatarDataURI) => {
+  const token = (await Auth.currentSession()).getIdToken().getJwtToken();
+
+  const response = await axios.post(
+    `${API_URI}/avatars`,
+    { avatarDataURI },
+    { headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` } },
+  );
+  return response.data;
+  // if (response.status === 201 && response.data.success === 'true') return response.data;
+  // throw new Error(response.data);
+};
+
+interface DeleteUser {
+  (): Promise<string>;
+}
+
+const deleteUser: DeleteUser = async () => {
+  const user = (await Auth.currentUserPoolUser()) as CognitoUser;
+  return new Promise((resolve, reject) => {
+    user.deleteUser((err, result) => {
+      if (err) reject(new Error('Failed deleting user.'));
+      resolve(result as string);
+    });
+  });
+};
+
 const value = {
   signUp,
   updateUserAttributes,
   signIn,
+  verifyEmailAttribute,
   signInWithGoogle,
   signOut,
   getSession: () => Auth.currentSession(),
+  changePassword,
   forgotPasswordSendCode,
   forgotPasswordReset,
+  uploadAvatarOnSignUp,
+  uploadAvatar,
+  deleteUser,
 };
 
 const Context = createContext(value);
 
 const Provider: React.FunctionComponent = ({ children }) => {
   useEffect(() => {
-    Auth.currentSession().then((s) => console.log(s));
+    Auth.currentSession()
+      .then((s) => console.log(s))
+      .catch(() => {});
   }, []);
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
