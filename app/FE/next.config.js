403723f8da013efd87929f11a1c7294977acb7f2
mcrisan5537 <crisanmihai5537@gmail.com>
2021-05-24 21:59:35 +0300

    FE: Add babel & webpack rules for @react-three/drei, move react-three-fiber to @react-three/fiber

--- app/FE/next.config.js
+++ app/FE/next.config.js
@@ -1,9 +1,17 @@
+const path = require('path');
+
 // Custom webpack configuration
 module.exports = {
-  webpack(config) {
+  webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
+    });
+
+    config.module.rules.push({
+      test: /\.js$/,
+      use: defaultLoaders.babel,
+      include: [path.resolve(__dirname, 'node_modules', '@react-three', 'drei')],
     });
 
     return config;
