a6c9ec5b0d7693e9280f794d7a00f22b2101eb6b
mcrisan5537 <crisanmihai5537@gmail.com>
2021-05-28 22:25:50 +0300

    BE: Add users API

---
+++ app/BE/rest-api/users/get-user.ts
@@ -0,0 +1,34 @@
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { AdminGetUserResponse } from 'aws-sdk/clients/cognitoidentityserviceprovider';
+import { makeResponse, loadEnvironmentVariables } from '../shared';
+
+loadEnvironmentVariables();
+
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+
+const getUser = async (event: any) => {
+  console.log(event);
+
+  const userId = event.pathParameters.userId;
+
+  try {
+    const { $response } = await cognitoIdentityServiceProvider
+      .adminGetUser({
+        UserPoolId: process.env.USER_POOL_ID as string,
+        Username: userId,
+      })
+      .promise();
+    console.log($response);
+
+    const userAttributes: { [key: string]: string } = {};
+    for (const { Name, Value } of ($response.data as AdminGetUserResponse).UserAttributes!)
+      userAttributes[Name] = Value!;
+    return makeResponse(200, true, {
+      data: userAttributes,
+    });
+  } catch (e) {
+    return makeResponse(400, false, { error: e });
+  }
+};
+
+export { getUser };
---
+++ app/BE/rest-api/users/put-user.ts
@@ -0,0 +1,49 @@
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import * as JWT from 'jsonwebtoken';
+import { makeResponse, loadEnvironmentVariables } from '../shared';
+
+loadEnvironmentVariables();
+
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+
+const putUser = async (event: any) => {
+  console.log(event);
+
+  // ID token already validated by API Gateway, just decode it
+  const { 'cognito:username': userId } = JWT.decode(
+    event.headers.Authorization.split(' ')[1]
+  ) as any;
+
+  if (userId !== event.pathParameters.userId)
+    return makeResponse(401, false, { error: "You can only change your account's attributes." });
+
+  let requestBody;
+  try {
+    requestBody = JSON.parse(event.body);
+  } catch (e) {
+    return makeResponse(400, false, { error: e.message });
+  }
+
+  const userAttributes = [];
+  for (const [attributeName, attributeValue] of Object.entries(requestBody)) {
+    if (!attributeName.startsWith('custom'))
+      return makeResponse(403, false, { error: 'Only custom attributes are changeable.' });
+    userAttributes.push({ Name: attributeName, Value: attributeValue });
+  }
+
+  try {
+    const { $response } = await cognitoIdentityServiceProvider
+      .adminUpdateUserAttributes({
+        UserPoolId: process.env.USER_POOL_ID as string,
+        Username: userId,
+        UserAttributes: userAttributes as CognitoIdentityServiceProvider.AttributeListType,
+      })
+      .promise();
+    console.log($response);
+    return makeResponse(200, true, {});
+  } catch (e) {
+    return makeResponse(400, false, { error: e });
+  }
+};
+
+export { putUser };
