7121ef14f5fd64ffa9cbc7775b0498a6f7256e83
mcrisan5537 <crisanmihai5537@gmail.com>
2021-05-30 23:40:29 +0300

    FE: Add StageId to index page component

--- app/FE/src/pages/index.tsx
+++ app/FE/src/pages/index.tsx
@@ -3,21 +3,25 @@ import { Stage, ChatManager } from '#components/index';
 import { Header, StyledPageWrapper } from '#components/shared';
 import { SocketContext } from '#contexts';
 
-const Index = () => (
-  <>
-    <Head>
-      <title>Think-In</title>
-    </Head>
+const Index = () => {
+  const stageId = 'dev-room';
 
-    <Header />
+  return (
+    <>
+      <Head>
+        <title>Think-In</title>
+      </Head>
 
-    <StyledPageWrapper>
-      <SocketContext.Provider>
-        <Stage />
-        <ChatManager />
-      </SocketContext.Provider>
-    </StyledPageWrapper>
-  </>
-);
+      <Header />
+
+      <StyledPageWrapper>
+        <SocketContext.Provider stageId={stageId}>
+          <Stage />
+          <ChatManager />
+        </SocketContext.Provider>
+      </StyledPageWrapper>
+    </>
+  );
+};
 
 export default Index;
