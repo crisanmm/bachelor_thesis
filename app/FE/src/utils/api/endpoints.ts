318842361ab380b065a73754ac149fb0e37bbcb1
mcrisan5537 <crisanmihai5537@gmail.com>
2021-06-11 12:40:22 +0300

    FE: Centralize endpoints

--- app/FE/src/utils/api/endpoints.ts
+++ app/FE/src/utils/api/endpoints.ts
@@ -1,9 +1,18 @@
-const SCHEME_HOST = 'https://api.think-in.me';
-const STAGE = 'dev';
+const API_URL = 'https://api.think-in.me';
+const API_STAGE = 'dev';
 
-const API_ENDPOINTS = {
-  USERS: `${SCHEME_HOST}/${STAGE}/users`,
-  TRANSLATION: `${SCHEME_HOST}/${STAGE}/translation`,
+const S3_BUCKET_REGION = 'eu-central-1';
+const S3_CHATS_BUCKET_NAME = 'think-in-chats';
+const S3_STAGES_BUCKET_NAME = 'think-in-stages';
+
+const ENDPOINTS = {
+  API: `${API_URL}/${API_STAGE}`,
+  USERS: `${API_URL}/${API_STAGE}/users`,
+  STAGES: `${API_URL}/${API_STAGE}/stages`,
+  AVATARS: `${API_URL}/${API_STAGE}/avatars`,
+  TRANSLATION: `${API_URL}/${API_STAGE}/translation`,
+  S3_CHATS_BUCKET: `https://${S3_CHATS_BUCKET_NAME}.s3.${S3_BUCKET_REGION}.amazonaws.com`,
+  S3_STAGES_BUCKET: `https://${S3_STAGES_BUCKET_NAME}.s3.${S3_BUCKET_REGION}.amazonaws.com`,
 };
 
-export { API_ENDPOINTS };
+export { ENDPOINTS, S3_CHATS_BUCKET_NAME, S3_STAGES_BUCKET_NAME };
