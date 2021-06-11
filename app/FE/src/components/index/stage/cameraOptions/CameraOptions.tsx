e4510ee932dcb0e80d64db7eff916bb5af4d6819
mcrisan5537 <crisanmihai5537@gmail.com>
2021-06-11 12:23:46 +0300

    FE: Refactor OrbitControls into CameraOptions

--- app/FE/src/components/index/stage/cameraOptions/CameraOptions.tsx
+++ app/FE/src/components/index/stage/cameraOptions/CameraOptions.tsx
@@ -1,16 +1,49 @@
+import React, { SetStateAction, useEffect, useRef } from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
-import { useEffect, useRef } from 'react';
 import { PerspectiveCamera } from '@react-three/drei';
+import { Position } from '../shared';
 
-const CameraOptions = () => {
-  const perspectiveCameraRef = useRef<THREE.PerspectiveCamera>();
+interface CameraOptionsProps {
+  cameraPosition: Position;
+  cameraLookAt: Position;
+  orbitControls: any;
+  setOrbitControls: React.Dispatch<SetStateAction<any>>;
+}
+
+const CameraOptions: React.FunctionComponent<CameraOptionsProps> = ({
+  cameraPosition,
+  cameraLookAt,
+  orbitControls,
+  setOrbitControls,
+}) => {
+  const cameraRef = useRef<THREE.PerspectiveCamera>();
+  const {
+    gl: { domElement: canvasElement },
+  } = useThree();
+
   useEffect(() => {
-    perspectiveCameraRef.current?.lookAt(0, 0, 0);
+    cameraRef.current?.lookAt(new THREE.Vector3(...cameraLookAt));
   }, []);
 
-  return (
-    <PerspectiveCamera ref={perspectiveCameraRef} makeDefault fov={50} position={[-45, 30, 55]} />
-  );
+  useEffect(() => {
+    if (!orbitControls && cameraRef.current) {
+      (async () => {
+        const module = await import('three/examples/jsm/controls/OrbitControls');
+        const _controls = new module.OrbitControls(cameraRef.current!, canvasElement);
+        _controls.enableDamping = true;
+        _controls.enablePan = false;
+        _controls.target = new THREE.Vector3(...cameraLookAt);
+        setOrbitControls(_controls);
+      })();
+    }
+  }, [orbitControls]);
+
+  useFrame(() => {
+    if (orbitControls?.update) orbitControls.update();
+  });
+
+  return <PerspectiveCamera ref={cameraRef} makeDefault fov={50} position={cameraPosition} />;
 };
 
 export default CameraOptions;
