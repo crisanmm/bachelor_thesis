ae77cceb0d59d621b1002906057eb093365843ff
mcrisan5537 <crisanmihai5537@gmail.com>
2021-06-12 00:38:46 +0300

    FE: Add global styling to theme to hide scrollbars

--- app/FE/src/utils/theme/theme.ts
+++ app/FE/src/utils/theme/theme.ts
@@ -1,10 +1,24 @@
 import { lightPalette, darkPalette } from './palette';
 
+const baseTheme = {
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '::-webkit-scrollbar': {
+          display: 'none',
+        },
+      },
+    },
+  },
+};
+
 const lightTheme = {
+  ...baseTheme,
   palette: lightPalette,
 };
 
 const darkTheme = {
+  ...baseTheme,
   palette: darkPalette,
 };
 
